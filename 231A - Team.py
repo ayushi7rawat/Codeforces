0b169a45de78fcaaa3a2c46d68e9f53812f81a17
Ayushi Rawat <ayushi7rawat@gmail.com>
2021-05-26 07:45:49 +0530

    71A - Way Too Long Words

---
+++ 231A - Team.py
@@ -0,0 +1,9 @@
+'''
+231A - Team
+'''
+
+problems = int(input())
+
+for i in range(len(problems)):
+    temp = int(input)
+    data.append()
--- 71A - Way Too Long Words.py
+++ 71A - Way Too Long Words.py
@@ -1,6 +1,6 @@
-'''
-71A - Way Too Long Words
-'''
+# '''
+# 71A - Way Too Long Words
+# '''
 words = int(input())
 all_words = []
 
@@ -8,7 +8,7 @@ for word in range(words):
     temp = input()
     all_words.append(temp)
 
-# print(all_words)
+# # print(all_words)
 # all_words = ['word', 'localization', 'internationalization', 'pneumonoultramicroscopicsilicovolcanoconiosis']
 
 for i in range(len(all_words)):
@@ -16,5 +16,4 @@ for i in range(len(all_words)):
         temp = all_words[i]
         all_words[i] = temp[0] + str(len(temp[1:-1])) + temp[-1]
 
-for i in range(len(all_words)):
-        print(all_words[i])
+    print(all_words[i])
