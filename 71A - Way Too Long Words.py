7e180273478ebdcb349a438072d9df8c162ba90b
Ayushi Rawat <ayushi7rawat@gmail.com>
2021-05-26 07:23:12 +0530

    71A - Way Too Long Words

---
+++ 71A - Way Too Long Words.py
@@ -0,0 +1,20 @@
+'''
+71A - Way Too Long Words
+'''
+words = int(input())
+all_words = []
+
+for word in range(words):
+    temp = input()
+    all_words.append(temp)
+
+# print(all_words)
+# all_words = ['word', 'localization', 'internationalization', 'pneumonoultramicroscopicsilicovolcanoconiosis']
+
+for i in range(len(all_words)):
+    if len(all_words[i]) > 10:
+        temp = all_words[i]
+        all_words[i] = temp[0] + str(len(temp[1:-1])) + temp[-1]
+
+for i in range(len(all_words)):
+        print(all_words[i])
