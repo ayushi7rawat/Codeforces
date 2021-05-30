187bbe5b626906cb5fae768d2d8cb240369752e2
namasteayushi <namasteayushi@gmail.com>
2021-05-30 16:09:23 +0530

    4A- Watermelon

--- 231A - Team.py
+++ 231A - Team.py
@@ -3,7 +3,26 @@
 '''
 
 problems = int(input())
+data = []
 
-for i in range(len(problems)):
-    temp = int(input)
-    data.append()
+for i in range(problems):
+    temp = input()
+    data.append(temp)
+
+# data = ['1 1 0', '1 1 1', '0 0 1']
+# print(data)
+count = 0
+for i in range(len(data)):
+
+    temp = data[i].split()
+    print('temp', temp)
+
+    sum = 0
+
+    for i in range(len(temp)):
+        sum += int(temp[i])
+
+    if sum>1:
+        count += 1
+
+print(count)
--- 4A - Watermelon.py
+++ 4A - Watermelon.py
@@ -1,3 +1,7 @@
+'''
+4A- Watermelon
+By: Ayushi Rawat
+'''
 weight = int(input())
 
 if (weight %2 == 0) and weight >2:
